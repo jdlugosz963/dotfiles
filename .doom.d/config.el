5422ea7430f070323bbf8c6304567f248912781a
jdlugosz963 <jdlugosz963@gmail.com>
2021-12-19 17:12:03 +0100

    add my dot files

---
+++ .doom.d/config.el
@@ -0,0 +1,13 @@
+(setq user-full-name "Jakub Dluosz"
+      user-mail-address "jdlugosz963@gmail.com")
+
+(setq doom-theme 'doom-dracula)
+(setq doom-font (font-spec :family "Mononoki Nerd Font Mono" :size 13))
+
+(setq org-directory "~/org/")
+
+(setq display-line-numbers-type t)
+
+(setq display-line-numbers 'relative)
+
+(use-package! lsp-tailwindcss)
