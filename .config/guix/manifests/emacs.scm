79c31c7290e56f39982beed308d4b13a3d7e3c8c
jdlugosz963 <jdlugosz963@gmail.com>
2022-10-02 16:55:35 +0200

    Add emacs manifest.

---
+++ .config/guix/manifests/emacs.scm
@@ -0,0 +1,76 @@
+(specifications->manifest
+ '("emacs"
+   "emacs-exwm"
+   ;;"emacs-native-comp"
+   
+   "emacs-general"
+   
+   "emacs-undo-tree"
+   "emacs-evil"
+   "emacs-evil-collection"
+   
+   "emacs-diminish"
+   "emacs-doom-themes"
+   "font-hack"
+   
+   "emacs-hl-todo"
+   
+   "emacs-counsel"
+   "emacs-ivy"
+   
+   "emacs-which-key"
+   
+   "emacs-doom-modeline"
+   
+   "emacs-hydra"
+   
+   "emacs-beacon"
+   
+   "emacs-org"
+   
+   "emacs-evil-org"
+   
+   "emacs-toc-org"
+   
+   "emacs-visual-fill-column"
+   
+   "emacs-org-superstar"
+   
+   "emacs-org-roam"
+   
+   "emacs-lsp-mode"
+   "emacs-lsp-ivy"
+   
+   "emacs-pyvenv"
+   ;; "emacs-python-mode"
+   
+   "emacs-typescript-mode"
+   "emacs-web-mode"
+   "emacs-tide"
+   "emacs-flycheck"
+   
+   "emacs-yaml-mode"
+   
+   "emacs-docker"
+   
+   "emacs-company"
+   
+   "emacs-projectile"
+   
+   "emacs-neotree"
+   
+   "emacs-magit"
+   
+   "emacs-vterm"
+   
+   ; "emacs-dired-single"
+   "emacs-dired-hacks"
+   "emacs-all-the-icons-dired"
+   
+   "emacs-emms"
+   
+   "emacs-pdf-tools"
+   
+   "emacs-elfeed"
+   
+))
