55edbf585bba878eabd779d3e275c99ce1babd4b
jdlugosz963 <jdlugosz963@gmail.com>
2022-10-02 13:24:25 +0200

    Init guix config files

---
+++ .config/guix/machines/viking.scm
@@ -0,0 +1,29 @@
+(define-module (viking)
+  #:use-module (base-system)
+  #:use-module (gnu))
+(use-modules (base-system))
+(operating-system
+ (inherit base-operating-system)
+ (host-name "viking")
+
+ (keyboard-layout (keyboard-layout "pl"))
+
+ (bootloader
+  (bootloader-configuration
+   (bootloader grub-bootloader)
+   (target "/dev/sda")))
+
+ (mapped-devices
+  (list (mapped-device
+         (source
+          (uuid "75a1ebd7-13c2-442f-80ff-1b5e59522e29"))
+         (target "cryptroot")
+         (type luks-device-mapping))))
+
+ (file-systems
+  (cons* (file-system
+          (mount-point "/")
+          (device "/dev/mapper/cryptroot")
+          (type "ext4")
+          (dependencies mapped-devices))
+         %base-file-systems)))
