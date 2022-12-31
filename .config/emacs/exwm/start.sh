a3ab59de884a372226432ba50dd3c36e354ff22e
jdlugosz963 <jdlugosz963@gmail.com>
2022-12-31 23:20:53 +0100

    Update Desktop config

--- .config/emacs/exwm/start.sh
+++ .config/emacs/exwm/start.sh
@@ -4,7 +4,9 @@ picom &
 
 xss-lock -- slock &
 
-export VISUAL=emacsclient
+export VISUAL="emacsclient -a vim"
 export EDITOR="$VISUAL"
+export GTK_THEME="Adwaita:dark"
+export CALIBRE_USE_DARK_PALETTE=1
 
 exec dbus-launch --exit-with-session emacs -mm --debug-init -l ~/.config/emacs/exwm/desktop.el
