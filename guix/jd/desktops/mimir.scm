76187138be10009bd39ab1bfc50aecde6516f14f
jdlugosz963 <jdlugosz963@gmail.com>
2023-07-17 00:15:49 +0200

    Change mimir config.

--- guix/jd/desktops/mimir.scm
+++ guix/jd/desktops/mimir.scm
@@ -1,35 +1,36 @@
 (define-module (jd desktops mimir)
   #:use-module (jd desktops base)
   #:use-module (gnu)
-  #:use-module (gnu services desktop))
+  #:use-module (gnu home))
 
+(define home
+  (home-environment
+   (services %jd-base-home-services)))
 
-(operating-system
- (inherit odin-non-free)
- (host-name "mimir")
+(define system
+  (operating-system
+   (inherit odin-non-free)
+   (host-name "mimir")
 
- (services (cons
-	    (service xfce-desktop-service-type)
-	    %jd-base-services))
- 
- (bootloader (bootloader-configuration
-	      (bootloader grub-efi-bootloader)
-	      (targets (list "/boot/efi"))))
+   (bootloader (bootloader-configuration
+		(bootloader grub-efi-bootloader)
+		(targets (list "/boot/efi"))))
 
- 
- (swap-devices (list (swap-space
-		      (target (uuid
-			       "658793cb-d374-426e-bcd5-00d032b003a0")))))
+   (swap-devices (list (swap-space
+			(target (uuid
+				 "658793cb-d374-426e-bcd5-00d032b003a0")))))
 
- (file-systems (cons* (file-system
-		       (mount-point "/boot/efi")
-		       (device (uuid "64D8-134F"
-                                     'fat32))
-		       (type "vfat"))
-		      (file-system
-		       (mount-point "/")
-		       (device (uuid
-                                "1f9304b0-5623-4248-ab66-534b5ac85876"
-                                'ext4))
-		       (type "ext4")) %base-file-systems)))
+   (file-systems (cons* (file-system
+			 (mount-point "/boot/efi")
+			 (device (uuid "64D8-134F"
+                                       'fat32))
+			 (type "vfat"))
+			(file-system
+			 (mount-point "/")
+			 (device (uuid
+                                  "1f9304b0-5623-4248-ab66-534b5ac85876"
+                                  'ext4))
+			 (type "ext4")) %base-file-systems))))
+
+(if (getenv "JD_HOME") home system)
 
